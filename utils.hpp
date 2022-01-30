cd868f8310be9cd68cdcbdaafc140731568432a9
Txema Ynzenga <txemaynzenga@gmail.com>
2022-01-30 17:38:01 +0100

    Fix buggy utils::Buffer implementation
    and minor refactoring

--- utils.hpp
+++ utils.hpp
@@ -4,7 +4,7 @@
 #include <array>
 
 namespace utils {
-	static double midi_to_note_freq(int note) {
+	[[nodiscard]] static double midi_to_note_freq(int note) {
 		//Calculate difference in semitones to A4 (note number 69) and use equal temperament to find pitch.
 		return 440 * std::pow(2, ((double)note - 69) / 12);
 	}
@@ -15,7 +15,7 @@ namespace utils {
 		return res;
 	}();
 	template<typename T1, typename T2>
-	T2 mapToRange(T1 value, T2 rangeMin, T2 rangeMax) {
+	[[nodiscard]] T2 mapToRange(T1 value, T2 rangeMin, T2 rangeMax) {
 		T2 center = (rangeMin + rangeMax)/2;
 		T2 halfRange = std::abs(rangeMax - center);
 		return center + value * halfRange;
@@ -27,19 +27,20 @@ namespace utils {
 	private:
 		std::array<T, N> arr;
 		IteratorType inputIterator = arr.begin(), outputIterator = arr.begin();
-		IteratorType next(IteratorType it) {
+		[[nodiscard]] IteratorType next(IteratorType it) {
 			if (++it == arr.end()) it = arr.begin();
 			return it;
 		}
 	public:
 		std::size_t size = 0;
+		const std::size_t capacity = N;
 
 		[[nodiscard]] inline bool empty() {
-			return outputIterator == inputIterator;
+			return size == 0;
 		}
 
 		[[nodiscard]] inline bool full() {
-			return next(inputIterator) == outputIterator;
+			return size == N;
 		}
 
 		void push(T t) {
