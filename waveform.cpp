a796d771b9425ed89fbb4dc81fba0528947dd49d
Txema Ynzenga <txemaynzenga@gmail.com>
2025-07-26 19:02:01 +0200

    Add wavetable for square wave to avoid aliasing
    TODO: do one round of oversampling for the highest keys on the keyboard

--- waveform.cpp
+++ waveform.cpp
@@ -1,11 +1,47 @@
 #include "waveform.hpp"
 #include <cmath>
 #include <circle/timer.h>
+#include <array>
+#include <algorithm>
+#include <ranges>
+#include <numbers>
+
+static constexpr auto pi{std::numbers::pi_v<double>};
+
+template<std::size_t N>
+constexpr std::array<double, N> squareWaveCoefficients()
+{
+	std::array<double, N> result;
+	std::ranges::generate(result, [n = 0] mutable {double result; if (n % 2 == 0) result = 0; else result = 4./(pi * n); n++; return result;});
+	return result;
+}
+
+template<std::size_t N>
+constexpr std::array<double, N> squareWaveTable()
+{
+	static constexpr auto squareWaveCoeff{squareWaveCoefficients<10>()};
+	std::array<double, N> result{};
+	double freq{1.};
+	for (std::size_t i = 0; i < N; ++i)
+	{
+		double sample = 0;
+		for (const auto& [n, c] : squareWaveCoeff | std::views::enumerate)
+		{
+			double harmonicFreq{freq * n};
+			sample += c * std::sin(2.*pi*harmonicFreq*(static_cast<double>(i) / double {N}));
+		}
+		result[i] = sample;
+	}
+	return result;
+}
 
 double WaveformBase<0>::getSample(double t) {
-	double temp;
-	if (std::modf(f*t, &temp) < 0.5) return 1;
-	return -1;
+	// TODO: this still aliases at high enough fundamental frequencies
+	// Max harmonic = f * 10 = 1 * 10 = 10
+	// When downsampled to f = 2205 => max harmonic = 22050 = nyquist for 44100Hz sample rate
+	// One go of oversampling should be enough
+	static constexpr auto squareWaveTab{squareWaveTable<4096>()};
+	return squareWaveTab[std::size_t(f*t * squareWaveTab.size()) % squareWaveTab.size()];
 }
 
 double WaveformBase<1>::getSample(double t) {
@@ -21,4 +57,3 @@ double WaveformBase<3>::getSample(double t) {
 	double temp;
 	return sqrt(3)*(2*std::abs(2*std::modf(f*t, &temp) - 1) - 1);
 }
-
