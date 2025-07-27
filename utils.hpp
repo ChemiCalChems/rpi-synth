7ad1a17a068bb8084e0ca15cdea728b80d1d8e8f
Txema Ynzenga <txemaynzenga@gmail.com>
2025-07-27 21:09:00 +0200

    Make midi_freqs constexpr

--- utils.hpp
+++ utils.hpp
@@ -9,16 +9,17 @@
 namespace utils
 {
 
-[[nodiscard]] static double midi_to_note_freq(int note) {
+[[nodiscard]] static constexpr double midi_to_note_freq(int note) {
     //Calculate difference in semitones to A4 (note number 69) and use equal temperament to find pitch.
     return 440 * std::pow(2, ((double)note - 69) / 12);
 }
 
-const std::array<double, 128> midi_freqs = []() {
+static constexpr auto midi_freqs = []() {
     std::array<double, 128> res={0};
     for (unsigned i=0; i<res.size(); i++) res[i]=midi_to_note_freq(i);
     return res;
 }();
+
 template<typename T1, typename T2>
 [[nodiscard]] T2 mapToRange(T1 value, T2 rangeMin, T2 rangeMax) {
     T2 center = (rangeMin + rangeMax)/2;
